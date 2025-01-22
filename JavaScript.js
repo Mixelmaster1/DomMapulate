426a196fd3340db0d7efdb26cfec1aa32118872b
Xavier Jones <jonesxav002@warren.k12.in.us>
2025-01-22 12:57:15 -0500

    Initial commit

---
+++ JavaScript.js
@@ -0,0 +1,34 @@
+const header = document.getElementById("main-header");
+console.log(header);
+
+const firstParagraph = document.querySelector(selectors 'p');
+
+const allButtons= document.querySelectorAll(selectors 'button');
+
+console.log(allButtons);
+
+const item: = document.getElementsByClassName(classNames: 'list-item');
+
+console.log(items);
+
+const images = document.getElementByTagName(qualifeiedName: 'img');
+
+const newDiv: = cocument.createElement(tagName:'div');
+newDiv.textContent = 'Hello, World!';
+document.body.appendChild(newDiv);
+
+const heading: = document.querySelector(selectors 'h1');
+heading.textContent = 'Updated title';
+
+const list:NodeListOf<HTMLUListElement> = docment.querySelector(selectors:'ul');
+const firstItem = list.firstElementChild;
+const firstItem = list[0].lastElementChild;
+list[0].removeChild(firstItem);
+
+const button = docment.querySelector(selectors:'button');
+
+
+
+const sayHello = ():void => alert('Hello');
+button.addEventListener(type:)
+button.removeEventListener()
